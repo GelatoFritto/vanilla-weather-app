3b7ee6862aacc48b4ec8a3b0c229c193d2de6223
GelatoFritto <79608115+GelatoFritto@users.noreply.github.com>
2022-01-11 21:03:49 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,7 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d07c3a8ef0196e82f7eb5fda4ee394a0";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "d07c3a8ef0196e82f7eb5fda4ee394a0";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
